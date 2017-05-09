0b92006aa9caa8113b733a21bdb72b04e6c7fc93
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-05-08 20:25:32 -0400

    FIrst Commit

---
+++ raytracer.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <cstdio> 
+#include <cmath> 
+#include <fstream> 
+#include <vector> 
+#include <iostream> 
+#include <cassert> 
+
+int main (int argc, char **argv){
+    srand48(13); //initialize buffer of 13 psudo random integers
+
+}
