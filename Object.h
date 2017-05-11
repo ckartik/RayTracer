014aa72ee97972de09086aa61664c748e31cd391
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-05-11 19:57:31 -0400

    Removed duplicate code from object header file and fixed typo

--- Object.h
+++ Object.h
@@ -9,23 +9,8 @@ class Object {
     virtual Vec3<float> getColor();
     virtual Vec3 getNormalAt(Vec3 intersection_position);
     //TODO: @Kartik Change the pram type to Ray once Ray class implmented #REF2
-    virtual float findIntersection(Vec3 ray);i
+    virtual float findIntersection(Vec3 ray);
     virtual ~Object();
-}
-
-Object::Object () {}
-
-//TODO: @Kartik #REF1 here too
-virtual Vec3<float> Object::getColor(){
-    return Vec3 (0,0,0);
-}
-
-virtual float Object::findIntersection(Vec3 ray){
-    retur 0;
-}
-
-virtual Vec3<float> Object::getNormalAt(Vec3 intersection_position){
-    return Vec3 (0,0,0);
 }
 
 #endif
